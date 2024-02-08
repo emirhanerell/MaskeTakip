901d0300ac4d1fba3fb9cba1c95c57500ea87bb7
Emirhan EREL <emirhanerel@windowslive.com>
2024-02-08 15:42:10 +0300

    Maste Takip

---
+++ MaskeTakip/Business/Concrete/PttManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantSevice _appliantSevice; // new ile PersonManager'dan nesne üretmek yerine bunu kullanıyoruz. // Interface'in referanslarını alır
+        
+        public PttManager(IApplicantSevice appliantSevice) // Bu yapı ile hem yabancılar hem türkler için maske dağıtımı gerçekleşebiliyor. Bu gücü interface'ten alıyoruz.
+        {
+            _appliantSevice = appliantSevice;   // Ve biz bu sınıftan bir nesne üreteceğimiz zaman bizden ForeignerManager'dan mı yoksa PersonManager'dan mı diye soracak.
+        }
+        public void GiveMask(Person person)
+        {
+            if(_appliantSevice.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + person.LastName + " için maske verildi " );
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + person.LastName + " için maske verilemedi");
+            }
+        }
+    }
+}
---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,63 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vatandas vatandas = new Vatandas();
+            vatandas.Ad = "Emirhan";
+            SelamVer(isim: "Emirhan");
+
+            int sonuc = Topla(sayi1: 5, sayi2: 6);
+
+            List<string> ogrencilerr = new List<string>();
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+
+            foreach (string s in sehirler1)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            // -------- MaskeTakip projesi kodları --------
+
+            Person person1 = new Person();
+            person1.FirstName = "Emirhan";
+            person1.LastName = "EREL";
+            person1.DateOfBirthYear = 2002;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FirstName = "Eda";
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            
+        }
+        static void SelamVer(string isim)
+        {
+            Console.WriteLine("Merhaba" + isim);
+        }
+        static int Topla(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam: " + sonuc.ToString());
+            return sonuc;
+        }       
+    }
+  
+    // pascal casing 
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+    }
+   
+}
